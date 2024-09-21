ad538257df6ee33a76491173b3474cb6ce2752d0
kantber <aqweschen@yandex.ru>
2024-09-21 23:16:39 +0300

    добавил проект DEV_END

---
+++ DEV_END/RTE/_STM32F303/RTE_Components.h
@@ -0,0 +1,29 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'dev_end' 
+ * Target:  'STM32F303' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f3xx.h"
+
+/* Keil.STM32CubeMX::Device:STM32Cube Framework:STM32CubeMX:1.11.3 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/* Keil::CMSIS Driver:USART:USART Async:1.0.0 */
+#define RTE_Drivers_USART1_Async        /* Driver USART1 */
+        #define RTE_Drivers_USART2_Async        /* Driver USART2 */
+        #define RTE_Drivers_USART3_Async        /* Driver USART3 */
+        #define RTE_Drivers_USART4_Async        /* Driver USART4 */
+        #define RTE_Drivers_USART5_Async        /* Driver USART5 */
+
+
+#endif /* RTE_COMPONENTS_H */
